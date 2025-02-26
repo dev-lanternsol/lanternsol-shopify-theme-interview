ba9e56e2259ff030c62f8bd28da49a2e0334e403
dev-lanternsol <dev@lantensol.com>
2025-02-26 17:54:09 -0500

    Fixed ESLint undefined variable errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,22 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: { 
+      globals: {
+        ...globals.browser, // Keep existing browser globals
+        Shopify: "readonly",
+        throttle: "readonly",
+        trapFocus: "readonly",
+        removeTrapFocus: "readonly",
+        onKeyUpEscape: "readonly",
+        CartItems: "readonly",
+        debounce: "readonly",
+        ON_CHANGE_DEBOUNCE_TIMER: "readonly"
+      }
+    }
+  },
   pluginJs.configs.recommended,
 ];
